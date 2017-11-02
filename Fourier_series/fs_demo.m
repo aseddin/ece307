f5f27a5853c886601fd49e724207ca25e4c0796b
aseddin <asalaheddin@gmail.com>
2017-11-01 19:20:50 -0700

    Showing the effect of harmonic decay

--- Fourier_series/fs_demo.m
+++ Fourier_series/fs_demo.m
@@ -4,13 +4,13 @@ close all;
 
 
 % Coefficients as a matrix
-% Square wave 
+% % Square wave 
 w0 = pi; 
 a0 = 0.5;
 an = [0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0];
 bn =[0.6366         0    0.2122         0    0.1273         0    0.0909         0    0.0707         0    0.0579         0    0.0490         0    0.0424         0    0.0374         0    0.0335         0];
 
-% Sawtooth
+% % Sawtooth
 % w0 = 2 * pi / 3;
 % a0 = 3;
 % an = [0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0];
@@ -47,23 +47,36 @@ for n = 1:length(an)
     plot(t, s1);
     title('a_n cos(n \omega_0 t)');
     xlabel('t');
+    % fix the y axis limits to observe the harmonic decay with frequency
+    if(an(1) ~= 0) 
+        ylim([-an(1) an(1)]);
+    end
     
     % nth harmonic sin
     subplot(4, 1, 2);
     plot(t, s2); 
     title('b_n sin(n \omega_0 t)');
     xlabel('t');
+    % fix the y axis limits to observe the harmonic decay with frequency
+    if(bn(1) ~= 0) 
+        ylim([-bn(1) bn(1)]);
+    end
     
     % nth harmonic
     subplot(4, 1, 3);
     plot(t, s1 + s2); 
     title('a_n cos(n \omega_0 t) + b_n sin(n \omega_0 t)');
     xlabel('t');
+    % fix the y axis limits to observe the harmonic decay with frequency
+    if((an(1) ~= 0) || (bn(1) ~= 0)) 
+        ylim([-max(an(1), bn(1)) max(an(1), bn(1))]);
+    end
     
     % f reconstructed with n harmonics
     subplot(4, 1, 4);
     plot(t, f); 
     title(['f(t) reconstructed using', int2str(n), ' Harmonics']);
+
     
     pause;
     
